7a4ad5420140d169bc46604b6e97044c55363e03
osama-sayah <157955955+osama-sayah@users.noreply.github.com>
2024-01-28 22:55:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -42,21 +42,3 @@ function displayProducts(products) {
 //    .then(data => console.log(data.products));
 // });
 
-// function displayProducts(productsData) {
-//   const flexContainer = document.getElementById('flexContainer');
-
-//   // Clear existing flex items
-//   flexContainer.innerHTML = '';
-
-//   // Create flex items for each data item
-//   productsData.forEach(item => {
-//       const flexItem = document.createElement('div');
-//       flexItem.classList.add('flexItem');
-//       flexItem.innerHTML = `
-//           <p>ID: ${item.id}</p>
-//           <p>Name: ${item.name}</p>
-//           <!-- Add more content as needed -->
-//       `;
-//       flexContainer.appendChild(flexItem);
-//   });
-// }
